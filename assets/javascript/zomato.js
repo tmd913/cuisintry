6ae5e9d830e1a965abb922455a5858324c1fc3f9
Tom Dennis <tdennis913@gmail.com>
2018-09-06 15:13:44 -0400

    initial deployed version

--- assets/javascript/zomato.js
+++ assets/javascript/zomato.js
@@ -4,44 +4,26 @@ let accessToken = '1dec11522d7c1a36d57081a995c76d52';
 
 function displayCountries() {
 
-    var country = $("#countrySearch").val().trim();
-    console.log(country);
-    // var country = "dominica";
-    var queryURL = "https://restcountries.eu/rest/v2/name/" + country + "?fullText=true";
+    let country = $("#countrySearch").val().trim();
+
+    let queryURL = "https://restcountries.eu/rest/v2/name/" + country + "?fullText=true";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
 
-        console.log(response);
+        let flag = $("<img>").attr({ "src": response[0].flag, "id": "country-flag" }).css("height", "300px");
 
-        var name = response[0].name;
-        
-        var flag = $("<img>").attr("src", response[0].flag);
-    
-        var nativeName = response[0].nativeName;
-       
-
-        $(".col s6").empty();
-
-        $(".name").text("Name: " + response[0].name);
+        $(".card-image").html(flag);
+        $(".name").text(response[0].name);
         $(".capital").text("Capital: " + response[0].capital);
         $(".region").text("Region: " + response[0].region);
-        $(".population").text("Population: " + response[0].population);
+        $(".population").text("Population: " + response[0].population.toLocaleString('en'));
         $(".nativeName").text("Native name: " + response[0].nativeName);
         $(".currencies").text("Currency: " + response[0].currencies[0].name);
         $(".languages").text("Languages: " + response[0].languages[0].nativeName);
-
-        console.log(name);
-        console.log(nativeName);
-        console.log(response[0].flag);
-
-        $(".flag").html(flag);
-
     });
-
-
 }
 
 $(document).ready(function () {
@@ -54,15 +36,15 @@ $(document).ready(function () {
         $("#restaurantResults").empty();
         $(".pictures").empty();
         let country = $("#countrySearch").val().trim();
+        $("#wiki-link").attr("href", `https://en.wikipedia.org/wiki/${country}`);
         let zip = $("#restaurantSearch").val().trim();
         let cuisineID;
         let lat;
         let lng;
+        let page = 1
         displayCountries();
         $.getJSON("https://raw.githubusercontent.com/tmd913/project0/master/assets/json/country_cuisine_id_lu.json", json => {
-            // console.log(json[country]);
             cuisineID = json[country].CUISINE_ID;
-            // console.log(cuisineID);
 
             // ================================
             // zip code api for later use
@@ -78,11 +60,31 @@ $(document).ready(function () {
             // ================================
 
             $.getJSON("https://raw.githubusercontent.com/tmd913/project0/master/assets/json/us_zip_to_lat_long_lu.json", json => {
-                // console.log(json[zip]);
                 lat = json[zip].LAT;
                 lng = json[zip].LNG;
-                // console.log(lat);
-                // console.log(lng);
+
+                $.get(`https://www.lonelyplanet.com/${country}/places?page=${page}`, response => {
+                    let placeLink = $(response).find(".card__mask > a");
+
+                    for (const key in placeLink) {
+                        if (placeLink.hasOwnProperty(key)) {
+                            const element = placeLink[key];
+                            if ($(element).attr("href") != undefined) {
+                                let link = $(element).attr("href")
+                                $.get(`https://www.lonelyplanet.com/${link}`, response2 => {
+                                    if ($(response2).find(".slideshow__slide")[0] != undefined) {
+                                        let title = $(response2).find(".masthead__title")[0].innerText;
+                                        let slideImg = $(response2).find(".slideshow__slide")[0].style.backgroundImage;
+                                        let div = $('<div class="lp-img-container">');
+                                        div.append($("<a>").attr({ "href": `https://www.lonelyplanet.com/${link}`, "target": "_blank", "class": "place-link" }).text(title.toUpperCase()));
+                                        div.append($("<img>").attr({ "src": slideImg.substring(5, slideImg.length - 2), "height": "178px", "width": "275px" }));
+                                        $(".pictures").append(div);
+                                    }
+                                });
+                            }
+                        }
+                    }
+                });
 
                 $.ajax({
                     url: `https://developers.zomato.com/api/v2.1/search?&lat=${lat}&lon=${lng}&cuisines=${cuisineID}&sort=rating&order=desc`,
@@ -90,42 +92,14 @@ $(document).ready(function () {
                         'user-key': accessToken,
                     },
                     success: response => {
-                        // console.log(response);
                         for (const key in response.restaurants) {
                             if (response.restaurants.hasOwnProperty(key)) {
                                 const element = response.restaurants[key];
                                 let restaurant = element.restaurant;
-                                // ================================
-                                // original zomato display info
-                                // ================================
-                                // console.log(restaurant);
-                                // let name = $("<h5>").text(restaurant.name);
-                                // let locality = $("<p>").text(restaurant.location.locality);
-                                // let address = $("<p>").text(restaurant.location.address);
-                                // let ratingScore = $("<p>").text(`${restaurant.user_rating.aggregate_rating}/5 from ${restaurant.user_rating.votes} user ratings - ${restaurant.user_rating.rating_text}`);
-                                // let ratingText = $("<p>").text(restaurant.user_rating.rating_text);
-                                // let ratingVotes = $("<p>").text(`${restaurant.user_rating.votes} votes`);
-                                // let cuisines = $("<p>").text(`Cuisines: ${restaurant.cuisines}`);
-                                // let costFor2 = $("<p>").text(`Average Cost For Two: ${restaurant.currency}${restaurant.average_cost_for_two}`);
-                                // let url = $("<a>").attr({ "href": restaurant.url, "target": "_blank" }).text(`Zomato Page`);
-                                // let restaurantContainer = $('<div class="restaurant-container">').css({ "border": "solid black 1px", "padding": "10px", "margin": "10px" });
-                                // $(restaurantContainer).append(name);
-                                // $(restaurantContainer).append(locality);
-                                // $(restaurantContainer).append(address);
-                                // $(restaurantContainer).append(ratingScore);
-                                // $("#restaurantResults").append(ratingText);
-                                // $("#restaurantResults").append(ratingVotes);
-                                // $(restaurantContainer).append(cuisines);
-                                // $(restaurantContainer).append(costFor2);
-                                // $(restaurantContainer).append(url);
-                                // $("#restaurantResults").append(restaurantContainer);
-                                // ================================
-                                // end original zomato display info
-                                // ================================
                                 let restaurantContainer = $(
                                     `<div class="row">
                                         <div class="col s12">
-                                            <div class="card teal darken-1">
+                                            <div class="card darken-1" id="restaurant-card">
                                                 <div class="card-content white-text">
                                                     <span class="card-title">${restaurant.name}</span>
                                                     <p>${restaurant.location.locality}</p>
@@ -142,35 +116,8 @@ $(document).ready(function () {
                                     </div>`
                                 );
                                 $("#restaurantResults").append(restaurantContainer);
-                                // console.log(restaurantContainer);
-
-                                for (let i = 1; i < 6; i++) {
-                                    $.get(`https://www.lonelyplanet.com/${country}/places?page=${i}`, response => {
-                                        let placeLink = $(response).find(".card__mask > a");
-
-                                        for (const key in placeLink) {
-                                            if (placeLink.hasOwnProperty(key)) {
-                                                const element = placeLink[key];
-                                                if ($(element).attr("href") != undefined) {
-                                                    let link = $(element).attr("href")
-                                                    $.get(`https://www.lonelyplanet.com/${link}`, response2 => {
-                                                        if ($(response2).find(".slideshow__slide")[0] != undefined) {
-                                                            let title = $(response2).find(".masthead__title")[0].innerText;
-                                                            let slideImg = $(response2).find(".slideshow__slide")[0].style.backgroundImage;
-                                                            let div = $("<div>");
-                                                            div.append($("<a>").attr({ "href": `https://www.lonelyplanet.com/${link}`, target: "_blank" }).text(title));
-                                                            div.append($("<img>").attr({ "src": slideImg.substring(5, slideImg.length - 2), "height": "162px", "width": "250px" }));
-                                                            $(".pictures").append(div);
-                                                        }
-                                                    });
-                                                }
-                                            }
-                                        }
-                                    });
-                                }
                             }
                         }
-
                     }
                 })
             });
