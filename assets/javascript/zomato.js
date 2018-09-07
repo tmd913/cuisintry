50c0d569de11e148e3095844fcd13c6fede9671e
Tom Dennis <tdennis913@gmail.com>
2018-09-07 11:21:36 -0400

    added switch to account for additional countries with country api

--- assets/javascript/zomato.js
+++ assets/javascript/zomato.js
@@ -1,6 +1,6 @@
-// let accessToken = '6816158a6fbe7ceacf3a9617a5853775';
+// extra useable access token for zomato api
+// let accessToken = '6816158a6fbe7ceacf3a9617a5853775'; 
 let accessToken = '1dec11522d7c1a36d57081a995c76d52';
-// let zipAccessToken = 'POxFNCPw1BNNVCYVJdmoM1laaANt2ocOUdhA86z4RcyYea2Eo6hhzQIhaNEyJdlK';
 
 let country;
 let zip;
@@ -13,7 +13,56 @@ const ajaxCalls = [];
 
 // calls country api to dynamically display country information
 function displayCountries() {
-    let queryURL = "https://restcountries.eu/rest/v2/name/" + country + "?fullText=true";
+    // account for some of the specific country names needed for country api
+    let countryAPI_string;
+    switch (country) {
+        case "bolivia":
+            countryAPI_string = "Bolivia (Plurinational State of)";
+            break;
+        case "iran":
+            countryAPI_string = "Iran (Islamic Republic of)";
+            break;
+        case "laos":
+            countryAPI_string = "Lao People's Democratic Republic";
+            break;
+        case "north korea":
+            countryAPI_string = "Korea (Democratic People's Republic of)";
+            break;
+        case "palestine":
+            countryAPI_string = "Palestine, State of";
+            break;
+        case "russia":
+            countryAPI_string = "Russian Federation";
+            break;
+        case "south korea":
+            countryAPI_string = "Korea (Republic of)";
+            break;
+        case "syria":
+            countryAPI_string = "Syrian Arab Republic";
+            break;
+        case "tanzania":
+            countryAPI_string = "Tanzania, United Republic of";
+            break;
+        case "england":
+        case "great britain":
+        case "britain":
+        case "northern ireland":
+            countryAPI_string = "United Kingdom of Great Britain and Northern Ireland";
+            break;
+        case "united states":
+            countryAPI_string = "United States of America";
+            break;
+        case "venezuela":
+            countryAPI_string = "Venezuela (Bolivarian Republic of)";
+            break;
+        case "vietnam":
+            countryAPI_string = "Viet Nam";
+            break;
+        default:
+            countryAPI_string = country;
+            break;
+    }
+    let queryURL = "https://restcountries.eu/rest/v2/name/" + countryAPI_string + "?fullText=true";
 
     $.ajax({
         url: queryURL,
@@ -36,43 +85,43 @@ function displayCountries() {
 // scrapes lonely planet website to dynamically pull back first page of images from countrys places page
 function displayLonelyPlanet() {
     // for (let page = 1; page < 6; page++) {
-        $.get(`https://www.lonelyplanet.com/${country}/places?page=${page}`, response => {
-            // finds links to places for specific country
-            let placeLink = $(response).find(".card__mask > a");
+    $.get(`https://www.lonelyplanet.com/${country}/places?page=${page}`, response => {
+        // finds links to places for specific country
+        let placeLink = $(response).find(".card__mask > a");
 
-            // loops through each link element found on places page
-            for (const key in placeLink) {
-                // only properties that are not inherited
-                if (placeLink.hasOwnProperty(key)) {
-                    const element = placeLink[key];
-                    // limits to elements with valid urls
-                    if ($(element).attr("href") != undefined) {
-                        // use to later build full place page link
-                        let link = $(element).attr("href")
-                        // adds ajax call to array so that they can later be stopped if taking too long to finish
-                        ajaxCalls.push($.get(`https://www.lonelyplanet.com/${link}`, response2 => {
-                            // limits to places with images available
-                            if ($(response2).find(".slideshow__slide")[0] != undefined) {
-                                // pulls lonely planet title from place page
-                                let title = $(response2).find(".masthead__title")[0].innerText;
-                                // pulls background image
-                                let slideImg = $(response2).find(".slideshow__slide")[0].style.backgroundImage;
-                                let div = $('<div class="lp-img-container">');
-                                let divLink = $('<div class="lp-link-container">');
-                                // builds full link to place page
-                                divLink.append($("<a>").attr({ "href": `https://www.lonelyplanet.com/${link}`, "target": "_blank", "class": "place-link" }).text(title.toUpperCase()));
-                                div.append(divLink);
-                                // extracts useable url from css string
-                                div.append($("<img>").attr({ "src": slideImg.substring(5, slideImg.length - 2), "height": "162px", "width": "250px" }));
-                                // stores newly created divs in array for potentially limited display arrangements
-                                lonelyPlanetImages.push(div);
-                                $(".pictures").append(div);
-                            }
-                        }));
-                    }
+        // loops through each link element found on places page
+        for (const key in placeLink) {
+            // only properties that are not inherited
+            if (placeLink.hasOwnProperty(key)) {
+                const element = placeLink[key];
+                // limits to elements with valid urls
+                if ($(element).attr("href") != undefined) {
+                    // use to later build full place page link
+                    let link = $(element).attr("href")
+                    // adds ajax call to array so that they can later be stopped if taking too long to finish
+                    ajaxCalls.push($.get(`https://www.lonelyplanet.com/${link}`, response2 => {
+                        // limits to places with images available
+                        if ($(response2).find(".slideshow__slide")[0] != undefined) {
+                            // pulls lonely planet title from place page
+                            let title = $(response2).find(".masthead__title")[0].innerText;
+                            // pulls background image
+                            let slideImg = $(response2).find(".slideshow__slide")[0].style.backgroundImage;
+                            let div = $('<div class="lp-img-container">');
+                            let divLink = $('<div class="lp-link-container">');
+                            // builds full link to place page
+                            divLink.append($("<a>").attr({ "href": `https://www.lonelyplanet.com/${link}`, "target": "_blank", "class": "place-link" }).text(title.toUpperCase()));
+                            div.append(divLink);
+                            // extracts useable url from css string
+                            div.append($("<img>").attr({ "src": slideImg.substring(5, slideImg.length - 2), "height": "162px", "width": "250px" }));
+                            // stores newly created divs in array for potentially limited display arrangements
+                            lonelyPlanetImages.push(div);
+                            $(".pictures").append(div);
+                        }
+                    }));
                 }
             }
-        });
+        }
+    });
     // }
 }
 
@@ -151,19 +200,6 @@ $(document).ready(function () {
         $.getJSON("https://raw.githubusercontent.com/tmd913/project0/master/assets/json/country_cuisine_id_lu.json", json => {
             cuisineID = json[country].CUISINE_ID;
             $("#restaurant-header").text(`LOCAL ${json[country].ADJ_FORM.toUpperCase()} RESTAURANTS`);
-
-            // ================================
-            // zip code api for later use
-            // ================================
-            // $.ajax({
-            //     url: 'https://www.zipcodeapi.com/rest/UR4aVihDZM5ZGru5FZN6ZUA4lxTFZTU61Xul2ii1bNa8AiIQEZLb4YKCLM4UYVrs/info.json/20036/degrees',
-            //     method: "GET"
-            // }).then( response => {
-            //     console.log(response);
-            // });
-            // ================================
-            // end zip code api for later use
-            // ================================
 
             //JSON that converts US zip codes to latitude and longitude so that we can interact with the Zomato API
             $.getJSON("https://raw.githubusercontent.com/tmd913/project0/master/assets/json/us_zip_to_lat_long_lu.json", json => {
